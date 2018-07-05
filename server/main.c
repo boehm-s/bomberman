1e36062046abd5b9c88cc736b3f462f14761ea5d
Steven BOEHM <boehm_s@etna-alternance.net>
2018-07-05 13:18:08 +0200

    dumb chat messaging

--- server/main.c
+++ server/main.c
@@ -1,5 +1,6 @@
 #include "includes/app_utils.h"
 
+#include "includes/socket_utils.h"
 #include "includes/server.h"
 #include "includes/map.h"
 #include "includes/player.h"
@@ -7,14 +8,34 @@
 
 
 int			main (int argc, char *argv []) {
+  t_game		*game;
+  struct sockaddr_in	server_addr;
+  long			port;
+  int			socket_fd;
+
   UNUSED(argc);
   UNUSED(argv);
-
-  t_game		*game;
-  game = init_game();
   UNUSED(game);
+
+  game = init_game();
   /* --------------------------------------------------------------------------------------------------------------- */
 
+
+  port = argc == 2 ? strtol(argv[1], NULL, 0) : PORT_NUMBER;
+
+  if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+      perror("Socket creation failed");
+      exit(1);
+  }
+
+  bind_socket(&server_addr, socket_fd, port);
+  if (listen(socket_fd, 1) == -1) {
+    perror("listen failed: ");
+    exit(1);
+  }
+
+  start_app(socket_fd);
+  close(socket_fd);
 
 
   /* --------------------------------------------------------------------------------------------------------------- */
